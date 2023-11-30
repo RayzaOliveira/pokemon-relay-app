fa9afbed2d59f61ff43052f8203d16c533a3c5a4
RayzaOliveira <rayza.ocr@gmail.com>
2023-11-30 09:57:25 -0300

    refactor: remove unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from './App.tsx'
-// import './index.css'
-import { PokemonList } from "./module/Pokemons/PokemonList/PokemonList";
 import { RelayEnvironmentProvider } from "react-relay";
 import environment from "./RelayEnvironment";
 import { Pokemons } from "./module/Pokemons/Pokemons";
